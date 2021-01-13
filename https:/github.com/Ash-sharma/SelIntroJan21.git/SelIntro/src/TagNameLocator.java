5e57361ec317ad5d29036d6b39c6c7b255012990
ash <ash@localhost>
2021-01-13 08:54:05 +0530

    first commit:  Uploading the whole project with tutorials for selenium

---
+++ https:/github.com/Ash-sharma/SelIntroJan21.git/SelIntro/src/TagNameLocator.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TagNameLocator {
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "/Users/ash/eclipse-workspace/SelIntro/Drivers/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.testandquiz.com/selenium/testing.html");
+		 
+		// by tagname
+		
+	WebElement inputField = driver.findElement(By.tagName("input"));
+	inputField.sendKeys("cars");
+		
+		
+	}
+}
