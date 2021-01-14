5e9717807d7f34a10371015b242028816c57285e
ash <ash@localhost>
2021-01-14 08:52:43 +0530

    Commiting the maven project

---
+++ SellProjJan/src/main/java/sel/FirstClass.java
@@ -0,0 +1,34 @@
+package sel;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "/Users/ash/eclipse-workspace/SellProjJan/Driver/chromedriver");
+		 
+		 WebDriver driver = new ChromeDriver();
+		 
+		 String url = "https://amazon.com";
+		 
+		 driver.get(url);
+		 String title = driver.getTitle();
+		 String expected = "Amazon.com";
+		 System.out.println(title);
+		 
+		 if (title.contains(expected)) {
+			 System.out.println("test passed");
+		 }
+			 else
+			 {System.out.println("failed");}
+		 
+			 
+			 
+		 
+		// to close the browser
+		 driver.close();
+	}
+
+	}
+
