969c782e426e11563cf61a67a39ace150c274eff
ash <ash@localhost>
2021-01-15 08:49:00 +0530

    updating with testNG

---
+++ SellProjJan/src/test/java/testNGIntro/FirstClass.java
@@ -0,0 +1,22 @@
+package testNGIntro;
+
+import org.testng.annotations.Test;
+
+public class FirstClass {
+
+	@Test
+ public void m1() {
+	System.out.println("Testing");
+		}
+	
+	@Test
+	public void m2() {
+		System.out.println("method 2 is running");
+			}
+
+	@Test
+	public void m3() {
+		System.out.println("method 3 is running");
+			}
+
+}
---
+++ SellProjJan/src/test/java/testNGIntro/SecondClass.java
@@ -0,0 +1,11 @@
+package testNGIntro;
+
+import org.testng.annotations.Test;
+
+public class SecondClass {
+	
+	@Test
+	public void m3() {
+		System.out.println("method 3 is running");
+			}
+}
